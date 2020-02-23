a9157ef12f3f33b8ed0dfb7acae25535b0047d6e
Yann <yannyavo@gmail.com>
2020-02-23 14:20:02 +0100

    add some routing test

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>The "Add product page</h1>')
+    res.send('<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
+
+app.post('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello From express</h1>')
 });
