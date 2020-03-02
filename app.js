a478481acb65a0ced4b62648ea5641bf5f8d9950
Yann <yannyavo@gmail.com>
2020-03-02 09:38:52 +0100

    routing added

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
-
-app.post('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello From express</h1>')
+app.use("/admin",adminRoutes);
+app.use(shopRoutes);
+app.use( (req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
