3e98f4d5fa988e512655ada4990b1eec502f2822
Yann <yannyavo@gmail.com>
2020-03-02 13:37:27 +0100

    Remove http page send

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).send();
 });
 
 app.listen(3000);
